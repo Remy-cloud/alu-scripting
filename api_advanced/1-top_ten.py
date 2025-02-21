ebfe28e70251c17f85c61e2073e90f89db5dfaf0
Remy-cloud <r.intwali@alustudent.com>
2025-02-21 22:42:34 +0200

    prints out 10 hot  post

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A function that prints the titles of the top
+10 hot posts from a subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    A function that fetches and prints the titles
+    of the top ten hot posts from a subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(
+        url,
+        params={"after": None},
+        allow_redirects=False,
+        headers=headers,
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    jsonData = response.json()
+    data = jsonData["data"]["children"]
+    for post in data:
+        print(post.get("data", {}).get("title"))
+
+
+# top_ten("programming")
